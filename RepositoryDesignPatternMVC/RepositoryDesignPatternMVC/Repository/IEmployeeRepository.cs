add25bd23be8a1f76050e7ff72e662051acfec90
Kristina Antoniuk <christina_antoniuk@outlook.com>
2019-09-09 22:15:39 +0300

    Initial commit. Implementing Repository Design Pattern using single DB table.

---
+++ RepositoryDesignPatternMVC/RepositoryDesignPatternMVC/Repository/IEmployeeRepository.cs
@@ -0,0 +1,15 @@
+﻿using RepositoryDesignPatternMVC.DAL;
+using System.Collections.Generic;
+
+namespace RepositoryDesignPatternMVC.Repository
+{
+    public interface IEmployeeRepository
+    {
+        IEnumerable<Employee> GetAll();
+        Employee GetById(int EmployeeID);
+        void Insert(Employee employee);
+        void Update(Employee employee);
+        void Delete(int EmployeeID);
+        void Save();
+    }
+}
